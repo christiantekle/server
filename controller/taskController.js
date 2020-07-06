f869ab4db21bc33c225f1547207e6ba314067d5c
christiantekle <christiantekle@users.noreply.github.com>
2020-07-06 19:30:45 +0400

    controller updated

--- controller/taskController.js
+++ controller/taskController.js
@@ -1,77 +1,70 @@
 const Task = require("../models/Task");
 
 const getTasks = async (ctx) => {
-    await Task.findAll()
-      .then((tasks) => {
-        ctx.body = tasks;
-      })
-      .catch((err) => {
-        ctx.body = " error: " + err;
-      });
-  };
-  const getTask = async (ctx) => {
-    await Task.findOne({
-      where: {
-        id: ctx.params.id,
-      },
-    })
-      .then((tasks) => {
-        ctx.body = tasks;
-      })
-      .catch((err) => {
-        ctx.body = "Task does not exist";
-      });
-  };
+  try {
+    const tasks = await Task.findAll();
+    ctx.body = tasks;
+  } catch (err) {
+    ctx.body = " error: " + err;
+  }
+};
 
-  const postTasks = async (ctx) => {
-    if (!ctx.request.body.task_name) {
-      ctx.body = {
-        error: "Bad Data",
-      };
-    } else {
-      await Task.create(ctx.request.body)
-        .then((data) => {
-          ctx.body = data;
-        })
-        .catch((err) => {
-          ctx.body = "error: " + err;
-        });
+const getTask = async (ctx) => {
+  try {
+    const tasks = await Task.findOne();
+    ctx.body = tasks;
+  } catch (err) {
+    ctx.body = "Task doesn't exist";
+  }
+};
+
+const postTasks = async (ctx) => {
+  if (!ctx.request.body.task_name) {
+    ctx.body = {
+      error: "Bad Data",
+    };
+  } else {
+    try {
+      const data = await Task.create(ctx.request.body);
+      ctx.body = data;
+    } catch (err) {
+      ctx.body = "error: " + err;
     }
-  };
+  }
+};
 
-  const deleteTask = async (ctx) => {
+const deleteTask = async (ctx) => {
+  try {
     await Task.destroy({
       where: {
         id: ctx.params.id,
       },
-    })
-      .then(() => {
-        ctx.body = { status: "Task Deleted" };
-      })
-      .catch((err) => {
-        ctx.body = "error: " + err;
-      });
+    });
+    ctx.body = { status: "Task Deleted" };
+  } catch (err) {
+    ctx.body = "error: " + err;
   }
-  const putTask = async (ctx) => {
-    if (!ctx.request.body.task_name) {
-      ctx.body = {
-        error: "Bad Data",
-      };
-    } else {
+};
+
+const putTask = async (ctx) => {
+  if (!ctx.request.body.task_name) {
+    ctx.body = {
+      error: "Bad Data",
+    };
+  } else {
+    try {
       await Task.update(
         { task_name: ctx.request.body.task_name },
         { where: { id: ctx.params.id } }
-      )
-        .then(() => {
-          ctx.body = { status: "Task Updated!" };
-        })
-        .catch((err) => {
-          ctx.body = "error: " + err;
-        });
+      );
+      ctx.body = { status: "Task Updated!" };
+    } catch (err) {
+      ctx.body = "error: " + err;
     }
   }
-  module.exports.getTasks = getTasks;
-  module.exports.getTask = getTask;
-  module.exports.postTasks = postTasks;
-  module.exports.deleteTask = deleteTask;
-  module.exports.putTask = putTask;
+};
+module.exports.getTasks = getTasks;
+module.exports.getTask = getTask;
+module.exports.postTasks = postTasks;
+module.exports.deleteTask = deleteTask;
+module.exports.putTask = putTask;
